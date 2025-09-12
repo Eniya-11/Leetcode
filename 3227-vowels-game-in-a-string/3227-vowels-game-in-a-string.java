ffb7854e5991a37e54f2b95d636c6c3046f84404
Eniya-11 <eniya460@gmail.com>
2025-09-12 13:38:05 +0530

    Time: 19 ms (16.8%), Space: 45.5 MB (66.93%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static boolean isVowel(char c){
+        c=Character.toLowerCase(c);
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+    public boolean doesAliceWin(String s) {
+        int vowelCount=0;
+        for(char c:s.toCharArray()){
+            if(isVowel(c)){
+                vowelCount++;
+            }
+        }
+        if(vowelCount==0){
+            return false;
+        }
+        return true;
+    }
+    
+}
